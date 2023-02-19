72198f7e8393e9b6fb07a1f1c16de36d2d9d4620
aj sth <aj.sth.2020@gmail.com>
2023-02-19 21:07:14 +0545

    made colorscheme file

--- init.lua
+++ init.lua
@@ -116,10 +116,6 @@ return require('lazy').setup({
 	{
 		'rose-pine/neovim',
 		as = 'rose-pine',
-		config = function()
-			require("rose-pine").setup()
-			vim.cmd('colorscheme rose-pine')
-		end
 	},
 	{ "catppuccin/nvim", name = "catppuccin" },
 	'folke/tokyonight.nvim',
