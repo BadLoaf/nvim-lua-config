7974a51b020425366a0a1e87b1738584f9ec1e34
aj sth <aj.sth.2020@gmail.com>
2023-02-27 12:42:06 +0545

    added noice.nvim

--- init.lua
+++ init.lua
@@ -110,9 +110,32 @@ return require('lazy').setup({
 	},
 
 	"lukas-reineke/indent-blankline.nvim",
+
+	-- setup for prettier
 	'neovim/nvim-lspconfig',
 	'jose-elias-alvarez/null-ls.nvim',
 	'MunifTanjim/prettier.nvim',
+
+	-- noice.nvim (optional, trying it out)
+
+	{
+		"folke/noice.nvim",
+		config = function()
+			require("noice").setup({
+				-- add any options here
+			})
+		end,
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		}
+	},
+
+
 
 	-- themes
 
